f67f04ce3abed0f3625a8c6118a0261ae73bc64c
sanskritiigarg <sanskritii.garg@gmail.com>
2024-08-22 14:18:11 +0530

    Parse arguments and read csv

---
+++ project.py
@@ -0,0 +1,24 @@
+import sys
+import csv
+
+def main():
+    infile, outfile = check_args(sys.argv)
+    males, females = map(list, ([],)*2)
+    with open(infile) as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            males.append(row) if row['gender'] == 'male' else females.append(row)
+    
+    print(males)
+    print(females)
+
+def check_args(args):
+    print(args)
+    if 1 < len(args) <= 3:
+        outfile = args[2] if len(args) == 3 else "match.csv"
+        return args[1], outfile
+    else:
+        sys.exit("Usage: python final_project.py input_file [output_file]")
+
+if __name__ == "__main__":
+    main()
