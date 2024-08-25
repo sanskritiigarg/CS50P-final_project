a90519066b5ff73676d146772cc3195a4d0dbe35
sanskritiigarg <sanskritii.garg@gmail.com>
2024-08-26 00:01:01 +0530

    Outputs matched csv correctly

--- project.py
+++ project.py
@@ -1,23 +1,54 @@
 import sys
+import random
 import csv
 
 def main():
     infile, outfile = parse_args(sys.argv)
-    males, females = map(list, ([],)*2)
+    males, females, bi = map(list, ([],)*3)
     with open(infile) as csvfile:
         reader = csv.DictReader(csvfile)
         for row in reader:
-            males.append(row) if row['gender'] == 'male' else females.append(row)
+            if row['preference'] == 'male/female':
+                bi.append(row)  
+            elif row['gender'] == 'female':
+                females.append(row)
+            else:
+                males.append(row)
+    output_csv(outfile, females, males, bi)
     
-    print(males)
-    print(females)
+
 
 def parse_args(args):
     if 1 < len(args) <= 3:
-        outfile = args[2] if len(args) == 3 else "match.csv"
+        outfile = args[2] if len(args) == 3 else 'match.csv'
         return args[1], outfile
     else:
-        sys.exit("Usage: python project.py input_file [output_file]")
+        sys.exit('Usage: python project.py input_file [output_file]')
+
+def output_csv(outfile, females, males, bi):
+    with open(outfile, 'w', newline='') as file:
+        writer = csv.DictWriter(file, fieldnames=['match', 'match_email', 'matched', 'matched_email'])
+        writer.writeheader()
+        for b in bi:
+            bi.remove(b)
+            match = random.choice([m for m in bi])
+            writer.writerow({'match': b['name'], 'match_email': b['email'], 'matched': match['name'], 'matched_email': match['email']})
+        for female in females:
+            if female['preference'] == 'male':
+                match = random.choice([m for m in males if female['gender'] == m['preference']])
+                males.remove(match)
+            if female['preference'] == 'female':
+                females.remove(female)
+                match = random.choice([f for f in females if female['gender'] == f['preference']])
+                females.remove(match)
+            writer.writerow({'match': female['name'], 'match_email': female['email'], 'matched': match['name'], 'matched_email': match['email']})
+        for male in males:
+            if male['preference'] == 'male':
+                males.remove(male)
+                match = random.choice(males)
+            writer.writerow({'match': male['name'], 'match_email': female['email'], 'matched': match['name'], 'matched_email': match['email']})
+
+
 
 if __name__ == "__main__":
     main()
