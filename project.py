2b7d36c5e24abee117754555d2b97c4e148a0314
sanskritiigarg <sanskritii.garg@gmail.com>
2024-08-29 22:18:45 +0530

    Functions and tests defined

--- project.py
+++ project.py
@@ -5,7 +5,7 @@ import csv
 UNMATCHED = []
 
 def main():
-    infile, outfile = parse_args(sys.argv)
+    infile, outfile= parse_args(sys.argv)
     males, females, bi = map(list, ([],)*3)
     
     #reading csv file
@@ -26,29 +26,36 @@ def main():
     with open(outfile, 'w', newline='') as file:
         writer = csv.DictWriter(file, fieldnames=['match', 'match_email', 'matched', 'matched_email'])
         writer.writeheader()
-        matches = match(bi)
 
-        for female in females:
-            if female['preference'] == 'male':
-                match = random.choice([m for m in males if female['gender'] == m['preference']])
-                males.remove(match)
-        if female['preference'] == 'female':
-            matches = match(females)
-        writer.writerow({'match': female['name'], 'match_email': female['email'], 'matched': match['name'], 'matched_email': match['email']})
-
-        for male in males:          
-            matches = match(males)    
+    #tabulate the csv    
+    if input("Do you want to output matched table in the terminal? Type 'Yes' or 'No': ").lower() == 'yes':
+        format = check_format(input("Enter Table format: ").lower())
+        table(outfile, format)
 
 def parse_args(args):
-    if not (1 < len(args) <= 3):
-        sys.exit('Usage: python project.py input_file [output_file]')
+    if not (len(args) == 3):
+        sys.exit('Usage: python project.py input_file output_file')
     if ".csv" not in args[1] or ".csv" not in args[2]:
          sys.exit("Only .csv files are supported.")
-    return args[1], args[2] if len(args) == 3 else "matches.csv"
+    return args[1], args[2]
 
 def match(candidates):
      ...
 
+def table(outfile, format):
+    return True
+    
+
+def check_format(format):
+    formats = ["plain", "simple", "github", "grid", "simple_grid", "rounded_grid", "heavy_grid", "mixed_grid", "textile"
+                "double_grid", "fancy_grid", "outline", "simple_outline", "rounded_outline", "heavy_outline", "mixed_outline"
+                "double_outline", "fancy_outline", "pipe", "orgtbl", "asciidoc", "jira", "presto", "pretty", "psql", "tsv"
+                "rst", "mediawiki", "moinmoin", "youtrack", "html", "unsafehtml", "latex", "latex_raw", "latex_booktabs", "latex_longtable"]
+    while True:
+        if format in formats:
+            return format
+        elif input("Table format is wrong. Do you want to Re-enter? Type 'Yes' or 'No': ").lower() != "yes":
+            sys.exit("Program exited.")
 
 if __name__ == "__main__":
     main()
--- test_project.py
+++ test_project.py
@@ -1,11 +1,26 @@
-from project import parse_args
+from project import parse_args, table, check_format
 import pytest
 
+
+
 def test_parse_args():
-    assert parse_args(['project.py', 'small_list.csv']) == ('small_list.csv', 'match.csv')
+    assert parse_args(['project.py', 'small_list.csv', 'match.csv']) == ('small_list.csv', 'match.csv')
     assert parse_args(['project.py', 'small_list.csv', 'matched.csv']) == ('small_list.csv', 'matched.csv')
 
     with pytest.raises(SystemExit) as exit:
         parse_args(['project.py'])
+        assert exit.type == SystemExit
+        assert exit.value.code == 1
+
+def test_table(monkeypatch):
+    assert table('matched.csv', 'plain') == True
+    assert table('matched.csv', 'simple_grid') == True
+    
+
+def test_check_format():
+    assert check_format('plain') == 'plain'
+
+    with pytest.raises(SystemExit) as exit:
+        parse_args('simple grid')
         assert exit.type == SystemExit
         assert exit.value.code == 1
