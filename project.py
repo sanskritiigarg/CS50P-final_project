67e9e3c55f7dbec87c5077eeb1e8933f9768c7b5
sanskritiigarg <sanskritii.garg@gmail.com>
2024-09-02 16:05:34 +0530

    Output CSV completed

--- project.py
+++ project.py
@@ -1,12 +1,13 @@
 import sys
 import random
 import csv
+from tabulate import tabulate
 
 UNMATCHED = []
 
 def main():
     infile, outfile= parse_args(sys.argv)
-    males, females, bi = map(list, ([],)*3)
+    sf, sm, bi, ga, lb = map(list, ([],)*5)
     
     #reading csv file
     try:
@@ -14,36 +15,70 @@ def main():
             reader = csv.DictReader(csvfile)
             for row in reader:
                 if row['preference'] == 'male/female':
-                    bi.append(row)  
-                elif row['gender'] == 'female':
-                    females.append(row)
-                else:
-                    males.append(row)
+                    bi.append(row)
+                if row["preference"] == "male" and row["gender"] == "female":
+                    sf.append(row)
+                if row["preference"] == "female" and row["gender"] == "male":
+                    sm.append(row)
+                if row["preference"] == "male" and row["gender"] == "male":
+                    ga.append(row)
+                if row["preference"] == "female" and row["gender"] == "female":
+                    lb.append(row)
     except FileNotFoundError:
          sys.exit("Couldn't read CSV file.")
 
     #writing output csv
-    with open(outfile, 'w', newline='') as file:
+    with open(outfile, 'w+', newline='') as file:
         writer = csv.DictWriter(file, fieldnames=['match', 'match_email', 'matched', 'matched_email'])
         writer.writeheader()
+        writer.writerows(match(bi))
+        try:
+            for f in sf:
+                try:
+                    matched = random.choice(sm)
+                    writer.writerow({"match": f["name"], "match_email": f["email"], "matched": matched["name"], "matched_email": matched["email"]})
+                except:
+                    UNMATCHED.append(sm)
+        except:
+            UNMATCHED.append(f)
+        writer.writerows(match(lb))
+        writer.writerows(match(ga))
 
-    #tabulate the csv    
-    if input("Do you want to output matched table in the terminal? Type 'Yes' or 'No': ").lower() == 'yes':
-        format = check_format(input("Enter Table format: ").lower())
-        table(outfile, format)
+        readers = csv.DictReader(file)
+        for row in readers:
+            print(row)
+        writer.writerows([unmatch for unmatch in UNMATCHED])
+    
+
 
 def parse_args(args):
     if not (len(args) == 3):
-        sys.exit('Usage: python project.py input_file output_file')
+        sys.exit("Usage: python project.py input_file output_file")
     if ".csv" not in args[1] or ".csv" not in args[2]:
          sys.exit("Only .csv files are supported.")
     return args[1], args[2]
 
 def match(candidates):
-     ...
+    random.shuffle(candidates)
+    n = len(candidates)
+    matched = []
+    if n % 2 == 1:
+        i = random.randint(0, n - 1)
+        UNMATCHED.append(candidates[i]) 
+        candidates.pop(i) 
+    for i in range(0,n, 2):        
+        matched.append(dict({"match": candidates[i]["name"], "match_email": candidates[i]["email"], "matched": candidates[i + 1]["name"], "matched_email": candidates[i + 1]["email"]}))
+    return matched
 
 def table(outfile, format):
-    return True
+    if input("Do you want to output matched table in the terminal? Type 'Yes' or 'No': ").lower() == "yes":
+        while True:
+            try:
+                format = input("Enter Table format: ").lower()
+                if check_format(format) == True:
+                    return True
+            except KeyboardInterrupt:
+                sys.exit("\nProgram Exited")
     
 
 def check_format(format):
@@ -51,11 +86,10 @@ def check_format(format):
                 "double_grid", "fancy_grid", "outline", "simple_outline", "rounded_outline", "heavy_outline", "mixed_outline"
                 "double_outline", "fancy_outline", "pipe", "orgtbl", "asciidoc", "jira", "presto", "pretty", "psql", "tsv"
                 "rst", "mediawiki", "moinmoin", "youtrack", "html", "unsafehtml", "latex", "latex_raw", "latex_booktabs", "latex_longtable"]
-    while True:
-        if format in formats:
-            return format
-        elif input("Table format is wrong. Do you want to Re-enter? Type 'Yes' or 'No': ").lower() != "yes":
-            sys.exit("Program exited.")
+    if format in formats:
+        return True
+    else:
+        return False
 
 if __name__ == "__main__":
     main()
