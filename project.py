ac1ebd08b604f786ad46c70ae5b4080746d96a08
sanskritiigarg <sanskritii.garg@gmail.com>
2024-09-04 21:20:45 +0530

    Completed

--- project.py
+++ project.py
@@ -9,7 +9,7 @@ def main():
     infile, outfile= parse_args(sys.argv)
     sf, sm, bi, ga, lb = map(list, ([],)*5)
     
-    #reading csv file
+    # reading csv file
     try:
         with open(infile) as csvfile:
             reader = csv.DictReader(csvfile)
@@ -27,28 +27,47 @@ def main():
     except FileNotFoundError:
          sys.exit("Couldn't read CSV file.")
 
-    #writing output csv
+    # writing output csv
     with open(outfile, 'w+', newline='') as file:
         writer = csv.DictWriter(file, fieldnames=['match', 'match_email', 'matched', 'matched_email'])
         writer.writeheader()
         writer.writerows(match(bi))
-        try:
-            for f in sf:
-                try:
-                    matched = random.choice(sm)
-                    writer.writerow({"match": f["name"], "match_email": f["email"], "matched": matched["name"], "matched_email": matched["email"]})
-                except:
-                    UNMATCHED.append(sm)
-        except:
-            UNMATCHED.append(f)
+        for f in sf:
+            try:
+                matched = random.choice(sm)
+                writer.writerow({"match": f["name"], "match_email": f["email"], "matched": matched["name"], "matched_email": matched["email"]})
+                sm.remove(matched)
+            except:
+                UNMATCHED.append(f)
+        if sm != []:
+            for m in sm:
+                UNMATCHED.append([m])
         writer.writerows(match(lb))
         writer.writerows(match(ga))
 
-        readers = csv.DictReader(file)
-        for row in readers:
-            print(row)
-        writer.writerows([unmatch for unmatch in UNMATCHED])
+    # create a table
+    format = ""
+    if input("Do you want to output matched table in the terminal? Type 'Y' or 'N': ").lower() == "y":
+        while True:
+            try:
+                format = input("Enter Table format: ").lower()
+                if check_format(format) == True:
+                    with open(outfile) as file:
+                        file.seek(0)
+                        reader = csv.reader(file)
+                        tables = [row for row in reader]
+                    table(tables, format)
+                    break
+            except KeyboardInterrupt:
+                    break
     
+    if UNMATCHED != []:
+        header = UNMATCHED[0].keys() 
+        rows = [x.values() for x in UNMATCHED]
+        print("\nUnmatched candidates:\n")
+        print(tabulate(rows, headers=header, tablefmt=format if format != "" else "grid"))
+    else:
+        print("\nNo unmatched candidates. All matched!")
 
 
 def parse_args(args):
@@ -63,24 +82,23 @@ def match(candidates):
     n = len(candidates)
     matched = []
     if n % 2 == 1:
-        i = random.randint(0, n - 1)
+        n -= 1
+        i = random.randint(0, n)
         UNMATCHED.append(candidates[i]) 
-        candidates.pop(i) 
-    for i in range(0,n, 2):        
-        matched.append(dict({"match": candidates[i]["name"], "match_email": candidates[i]["email"], "matched": candidates[i + 1]["name"], "matched_email": candidates[i + 1]["email"]}))
+        candidates.pop(i)
+    # base case if only two candidates left
+    if n == 2:
+        matched.append(dict({"match": candidates[0]["name"], "match_email": candidates[0]["email"], "matched": candidates[1]["name"], "matched_email": candidates[1]["email"]}))
+    else:
+        for i in range(0,n, 2):        
+            matched.append(dict({"match": candidates[i]["name"], "match_email": candidates[i]["email"], "matched": candidates[i + 1]["name"], "matched_email": candidates[i + 1]["email"]}))
     return matched
 
-def table(outfile, format):
-    if input("Do you want to output matched table in the terminal? Type 'Yes' or 'No': ").lower() == "yes":
-        while True:
-            try:
-                format = input("Enter Table format: ").lower()
-                if check_format(format) == True:
-                    return True
-            except KeyboardInterrupt:
-                sys.exit("\nProgram Exited")
+def table(tables, format):
+    print("\n", tabulate(tables[1:], headers=["name", "gender", "email", "preference"], tablefmt=format))
+    return True
+        
     
-
 def check_format(format):
     formats = ["plain", "simple", "github", "grid", "simple_grid", "rounded_grid", "heavy_grid", "mixed_grid", "textile"
                 "double_grid", "fancy_grid", "outline", "simple_outline", "rounded_outline", "heavy_outline", "mixed_outline"
--- test_project.py
+++ test_project.py
@@ -1,26 +1,20 @@
 from project import parse_args, table, check_format
 import pytest
 
-
-
 def test_parse_args():
-    assert parse_args(['project.py', 'small_list.csv', 'match.csv']) == ('small_list.csv', 'match.csv')
-    assert parse_args(['project.py', 'small_list.csv', 'matched.csv']) == ('small_list.csv', 'matched.csv')
+    assert parse_args(["project.py", "small_list.csv", "match.csv"]) == ("small_list.csv", "match.csv")
+    assert parse_args(["project.py", "small_list.csv", "matched.csv"]) == ("small_list.csv", "matched.csv")
 
     with pytest.raises(SystemExit) as exit:
-        parse_args(['project.py'])
+        parse_args(["project.py"])
         assert exit.type == SystemExit
         assert exit.value.code == 1
 
-def test_table(monkeypatch):
-    assert table('matched.csv', 'plain') == True
-    assert table('matched.csv', 'simple_grid') == True
-    
-
+def test_table():
+    assert table([['match', 'match_email', 'matched', 'matched_email'], ['Hermione Granger', 'hermionegranger@fiction.books', 'Peeta Mellark', 'peetamellark@fiction.books'], ['Ginny Wealsey', 'ginnyweasley@fiction.books', 'Ronald Weasley', 'ronweasley@fiction.books'], ['Katniss Everdeen', 'katnisseverdeen@fiction.books', 'Harry Potter', 'harrypotter@fiction.books'], ['Gellert Grindelwald', 'gellertgrindelwald@fiction.books', 'Albus Dumbledore', 'albusdumbledore@fiction.books']], 'plain') == True
+   
 def test_check_format():
-    assert check_format('plain') == 'plain'
+    assert check_format('plain') == True
+    assert check_format('simple grid') == False
 
-    with pytest.raises(SystemExit) as exit:
-        parse_args('simple grid')
-        assert exit.type == SystemExit
-        assert exit.value.code == 1
+    
